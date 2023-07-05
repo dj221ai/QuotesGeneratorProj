9591bf805dfb29b85ae4a72dc42168a216be8375
Ved Jha <vedprakash@fanclash.in>
2023-07-05 23:04:43 +0530

    quotes generator

---
+++ src/components/Quotes.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react";
+
+const Quotes = (props) => {
+    const [data, setData] = useState([]);
+    const [singleQuote, setSingleQuote] = useState();
+    let quote_url = 'https://type.fit/api/quotes';
+
+    useEffect (() => {
+        let mounted = true;
+        fetch(quote_url)
+        .then(response => response.json())
+        .then(jsondata => {
+            // console.log(jsondata)
+            if (mounted) {
+                setData(jsondata);
+                setSingleQuote(jsondata[0])
+            }
+        })
+        return () => mounted = false;
+    }, [])
+
+    const getQuotesData = () => {
+        setSingleQuote(data)
+        console.log("single quote >>>> ", singleQuote)
+    }
+
+    return (
+        <>
+            <button type="button" className="btn btn-primary" onClick={getQuotesData}>Generate Quote</button>
+            <h3>
+                <span>“</span>
+                {singleQuote?.text}
+                </h3>
+                <i>- {singleQuote?.author}</i>
+            {/* {data.map(
+                item => <p>{item.text}</p>
+            )} */}
+            <p>{singleQuote?.text}</p>
+
+        </>
+    )
+
+}
+
+export default Quotes;
+
